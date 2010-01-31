aa645862a863a9d376a292a77261f058ba933986
Pascal Rettig <pascal@cykod.com>
2010-01-30 19:32:43 -0500

    Added triggered actions

--- app/controllers/site_demo/page_controller.rb
+++ app/controllers/site_demo/page_controller.rb
@@ -2,7 +2,8 @@ class SiteDemo::PageController < ParagraphController
 
   editor_header 'Sitedemo Paragraphs'
   
-  editor_for :create_demo, :name => "Create demo", :feature => :site_demo_page_create_demo
+  editor_for :create_demo, :name => "Create demo", :feature => :site_demo_page_create_demo,
+    :triggers => [['Failed Request','failed'], ['Successful Req','action']]
 
   class CreateDemoOptions < HashModel
 
